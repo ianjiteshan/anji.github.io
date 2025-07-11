3ae09631e15154e751dbe4c7c20be112c1c8c7b1
ianjiteshan <anjitesh1976@gmail.com>
2025-07-11 17:14:48 +0530

    Alpha

--- script.js
+++
@@ -1,6 +0,0 @@
-// Example JavaScript: smooth scroll to project section
-document.querySelector('.cta-btn').addEventListener('click', (e) => {
-    e.preventDefault();
-    document.querySelector('#projects').scrollIntoView({ behavior: 'smooth' });
-  });
-  
